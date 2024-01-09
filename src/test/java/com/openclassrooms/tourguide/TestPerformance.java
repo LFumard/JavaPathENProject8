059a0360be4c0753ee1c46ce1ddd3333a653be87
LFumard <122115111+LFumard@users.noreply.github.com>
2024-01-09 15:08:21 +0100

    Test Github

--- src/test/java/com/openclassrooms/tourguide/TestPerformance.java
+++ src/test/java/com/openclassrooms/tourguide/TestPerformance.java
@@ -114,6 +114,7 @@ public class TestPerformance {
 		allUsers = tourGuideService.getAllUsers();
 		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
 
+
 		allUsers.forEach(u -> rewardsService.calculateRewards(u));
 
 		for(User user : allUsers) {

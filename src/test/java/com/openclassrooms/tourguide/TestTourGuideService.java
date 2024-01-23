160a4f04b7283de1bf197d8b4ad01c5b42057185
LFumard <122115111+LFumard@users.noreply.github.com>
2024-01-23 12:35:17 +0100

    Test Github

--- src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
+++ src/test/java/com/openclassrooms/tourguide/TestTourGuideService.java
@@ -32,7 +32,7 @@ public class TestTourGuideService {
 		tourGuideService.trackUserLocation(user);
 
 		try {
-			TimeUnit.MILLISECONDS.sleep(100);
+			TimeUnit.MILLISECONDS.sleep(1000);
 		} catch (InterruptedException e) {}
 
 		VisitedLocation visitedLocation = user.getLastVisitedLocation();
